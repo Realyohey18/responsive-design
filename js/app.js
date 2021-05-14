332a35718fd144619b3271150432569a266f0263
Realyohey18 <63124869+Realyohey18@users.noreply.github.com>
2021-05-14 20:07:32 +0700

    Update app.js
    I delete waste comments

--- js/app.js
+++ js/app.js
@@ -2,13 +2,12 @@ $(function(){
   
     //フロートヘッダーメニュー
     var targetHeight = $('.js-float-menu-target').height();
-    //heroバナー（section one）の高さを取得
-  console.log(targetHeight);//この場合、700px
+    //heroバナー（section one）の高さ取得
+  console.log(targetHeight);
     $(window).on('scroll', function() {
         $('.js-float-menu').toggleClass('float-active',$(this).scrollTop() > targetHeight);
     });
-     //⬆️説明：スクロールが７00px以上、下になった時に.js-float-menu(headerの部分)に.float-activeをつける。
-     //もしくはその反対の意味もある。
+    
      
     //spメニュー
     $('.js-toggle-sp-menu').on('click', function(){
